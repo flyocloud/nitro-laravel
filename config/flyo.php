45c83376116c2e04c34af2dfa642694e53544e7e
Nadar <git@nadar.io>
2024-08-24 08:47:11 +0000

    live edit and editable

--- config/flyo.php
+++ config/flyo.php
@@ -1,7 +1,12 @@
 <?php
 
 return [
+    // The token to authenticate the application with the Flyo API.
     'token' => env('FLYO_TOKEN', 'ADD_YOUR_TOKEN_HERE'),
+
+    // If enabled, the user can interact with the components, also it represents the application to be in development mode.
+    'live_edit' => env('FLYO_LIVE_EDIT', true),
+
+    // The namespace for the views, which means the views will be stored in resources/views/flyo.
     'views_namespace' => 'flyo',
-    'components_namespace' => 'flyo',
 ];
