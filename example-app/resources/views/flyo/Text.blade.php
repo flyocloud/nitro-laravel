492d48235a91f572a90aa9a9e6c37c943ff47101
Nadar <git@nadar.io>
2024-09-02 20:05:00 +0000

    Update HeroBanner and Text blade templates
    The HeroBanner and Text blade templates were updated to fix a typo in the inline style. The border color was changed from "gree" to "grey" in HeroBanner.blade.php, and from "gree" to "green" in Text.blade.php. This ensures consistent styling across the application.

--- example-app/resources/views/flyo/Text.blade.php
+++ example-app/resources/views/flyo/Text.blade.php
@@ -4,6 +4,6 @@
 use Flyo\Bridge\Wysiwyg;
 
 ?>
-<div @editable($block) style="border:1px solid gree; padding:20px;">
+<div @editable($block) style="border:1px solid green; padding:20px;">
     <?= Wysiwyg::render($block->getContent()->content->json); ?>
-<div>
+</div>
