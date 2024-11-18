936197715f84c6f9dcf56a7991f90f43ed31f109
Basil Suter <git@nadar.io>
2024-11-18 10:43:49 +0000

    add Flyo configuration options and update example environment file

--- example-app/config/flyo.php
+++ example-app/config/flyo.php
@@ -1,6 +1,26 @@
 <?php
 
 return [
-    'token' => env('FLYO_TOKEN', false),
-    'live_edit' => env('FLYO_LIVE_EDIT', true),
+    // The token to authenticate the application with the Flyo API.
+    // The production token can be set here, if there is no token set in the .env file, the production token will be used.
+    'token' => env('FLYO_TOKEN', 'ADD_PRODUCTION_TOKEN_HERE'),
+
+    // If enabled, the user can interact with the components, also it represents the application to be in development mode.
+    // The default setting false represents the application to be in production mode.
+    'live_edit' => env('FLYO_LIVE_EDIT', false),
+
+    // The namespace for the views, which means the views will be stored in resources/views/flyo.
+    'views_namespace' => 'flyo',
+
+    // TTL (Time-To-Live) for client-side cache headers, in seconds.
+    // Default is 900 seconds (15 minutes) its only availble if the liveEdit is disabled. Use 0 to disable client caching.
+    'server_cache_ttl' => env('FLYO_SERVER_CACHE_TTL', 900),
+
+    // TTL (Time-To-Live) for server-side cache headers, in seconds.
+    // Default is 1200 seconds (20 minutes) its only availble if the liveEdit is disabled. Use 0 to disable server caching.
+    'client_cache_ttl' => env('FLYO_CLIENT_CACHE_TTL', 1200),
+
+    // The default route to be used for the detail pages.
+    // Routes/Links can be defined in the Flyo interface for each corresponding entity, the default name is 'detail'.
+    'default_route' => env('FLYO_DEFAULT_ROUTE', 'detail'),
 ];
