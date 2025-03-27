1b7bc93b06f298ca8156cc9fac781101f9e5c076
Basil Suter <git@nadar.io>
2025-03-27 07:27:25 +0000

    refactor caching headers to conditionally set cache control headers based on configuration values

--- src/Middleware/CachingHeaders.php
+++ src/Middleware/CachingHeaders.php
@@ -19,10 +19,17 @@ class CachingHeaders
 
         if ($response->isSuccessful() && ! Env::get('APP_DEBUG') && ! $isLiveEdit) {
             $serverCacheTtl = $this->config->get('flyo.server_cache_ttl', 1200);
+            $serverCacheTtl = is_numeric($serverCacheTtl) ? (int) $serverCacheTtl : 0;
+            if ($serverCacheTtl > 0) {
+                $response->header('Vercel-CDN-Cache-Control', 'max-age='.$serverCacheTtl);
+                $response->header('CDN-Cache-Control', 'max-age='.$serverCacheTtl);
+            }
+
             $clientCacheTtl = $this->config->get('flyo.client_cache_ttl', 900);
-            $response->header('Vercel-CDN-Cache-Control', 'max-age='.$serverCacheTtl);
-            $response->header('CDN-Cache-Control', 'max-age='.$serverCacheTtl);
-            $response->header('Cache-Control', 'max-age='.$clientCacheTtl);
+            $clientCacheTtl = is_numeric($clientCacheTtl) ? (int) $clientCacheTtl : 0;
+            if ($clientCacheTtl > 0) {
+                $response->header('Cache-Control', 'max-age='.$clientCacheTtl);
+            }
         }
 
         return $response;
