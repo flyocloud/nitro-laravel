98deeff71897dfa5f4b14ce0c3da988850d6fdf3
Basil Suter <git@nadar.io>
2025-12-09 08:04:52 +0000

    Fix comments for cache TTL settings to clarify server and client configurations

--- config/flyo.php
+++ config/flyo.php
@@ -12,12 +12,14 @@ return [
     // The namespace for the views, which means the views will be stored in resources/views/flyo.
     'views_namespace' => 'flyo',
 
-    // TTL (Time-To-Live) for client-side cache headers, in seconds.
-    // Default is 900 seconds (15 minutes) its only availble if the liveEdit is disabled. Use 0 to disable client caching.
+    // TTL (Time-To-Live) for server-side cache headers, in seconds.
+    // Default is 900 seconds (15 minutes) its only availble if the liveEdit is disabled. Use 0 to disable server caching.
+    // will set Vercel-CDN-Cache-Control and CDN-Cache-Control headers
     'server_cache_ttl' => env('FLYO_SERVER_CACHE_TTL', 900),
 
-    // TTL (Time-To-Live) for server-side cache headers, in seconds.
-    // Default is 1200 seconds (20 minutes) its only availble if the liveEdit is disabled. Use 0 to disable server caching.
+    // The TTL for client-side cache headers, in seconds.
+    // Default is 1200 seconds (20 minutes) its only availble if the liveEdit is disabled. Use 0 to disable client caching.
+    // will set Cache-Control header
     'client_cache_ttl' => env('FLYO_CLIENT_CACHE_TTL', 1200),
 
     // The default route to be used for the detail pages.
