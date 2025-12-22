871eded82443e5f74f49acd28fa1f53da506514e
Basil Suter <git@nadar.io>
2025-12-22 08:24:15 +0000

    Revert "Apply CSP frame-ancestors middleware conditionally based on live edit status"
    This reverts commit 5f328036a0f04789b89e282be8fb62dbec17ce7e.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -105,12 +105,6 @@ class ServiceProvider extends SupportServiceProvider
             $isLiveEdit = $configRepository->get('flyo.live_edit', false);
             Log::debug('Flyo live edit is '.($isLiveEdit ? 'enabled' : 'disabled'));
 
-            // Apply CSP frame-ancestors only when live edit is enabled
-            $flyoMiddlewares = [CachingHeaders::class];
-            if ($isLiveEdit) {
-                $flyoMiddlewares[] = CspFrameAncestors::class;
-            }
-
             if ($isLiveEdit) {
                 // Keep page-refresh support
 
@@ -150,9 +144,9 @@ class ServiceProvider extends SupportServiceProvider
 JS);
             }
 
-            Route::get('/sitemap.xml', [SitemapController::class, 'render'])->middleware($flyoMiddlewares);
+            Route::get('/sitemap.xml', [SitemapController::class, 'render'])->middleware([CachingHeaders::class]);
 
-            Route::middleware('web')->group(function () use ($response, $config, $viewFactory, $flyoMiddlewares) {
+            Route::middleware('web')->group(function () use ($response, $config, $viewFactory) {
                 foreach ($response->getPages() as $page) {
                     Route::get($page, function () use ($page, $config, $viewFactory) {
                         $pageResponse = (new PagesApi(null, $config))->page($page, App::getLocale());
@@ -166,7 +160,7 @@ JS);
                         Head::metaImage($pageResponse->getMetaJson()->getImage());
 
                         return $viewFactory->make('cms', ['page' => $pageResponse]);
-                    })->middleware($flyoMiddlewares);
+                    })->middleware([CachingHeaders::class, CspFrameAncestors::class]);
                 }
             });
         }
